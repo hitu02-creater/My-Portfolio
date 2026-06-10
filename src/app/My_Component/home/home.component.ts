e5f99b7d7fbd559f361b3a68e4addd6c3fe1b585
Hitesh Vekariya <hiteshvekariya012@gmail.com>
2026-06-10 13:39:06 +0530

    change

--- src/app/My_Component/home/home.component.ts
+++ src/app/My_Component/home/home.component.ts
@@ -19,63 +19,63 @@ export class HomeComponent implements OnInit {
   charindex = 0;
   isDeleting = false;
 
-  ngOnInit(): void {
-    console.log('ngOnInit called');
-  this.startTyping();
-}
+//   ngOnInit(): void {
+//     console.log('ngOnInit called');
+//   this.startTyping();
+// }
 
-startTyping() {
-  setInterval(() => {
+// startTyping() {
+//   setInterval(() => {
+//     const currentWord = this.words[this.wordindex];
+
+//     if (!this.isDeleting) {
+//       this.displaytext = currentWord.substring(0, this.charindex++);
+//       if (this.charindex > currentWord.length) {
+//         this.isDeleting = true;
+//       }
+//     } else {
+//       this.displaytext = currentWord.substring(0, this.charindex--);
+//       if (this.charindex < 0) {
+//         this.isDeleting = false;
+//         this.wordindex = (this.wordindex + 1) % this.words.length;
+//         this.charindex = 0;
+//       }
+//     }
+//   }, 100);
+// }
+
+  ngOnInit(): void {
+    this.typeEffect();
+  }
+
+  typeEffect(): void {
     const currentWord = this.words[this.wordindex];
 
-    if (!this.isDeleting) {
-      this.displaytext = currentWord.substring(0, this.charindex++);
-      if (this.charindex > currentWord.length) {
-        this.isDeleting = true;
-      }
-    } else {
-      this.displaytext = currentWord.substring(0, this.charindex--);
+    if (this.isDeleting) {
+      this.displaytext = currentWord.substring(0, this.charindex - 1)
+      this.charindex--;
+
       if (this.charindex < 0) {
         this.isDeleting = false;
         this.wordindex = (this.wordindex + 1) % this.words.length;
         this.charindex = 0;
       }
     }
-  }, 100);
-}
 
-  // ngOnInit(): void {
-  //   this.typeEffect();
-  // }
+    else {
+      this.displaytext = currentWord.substring(0, this.charindex + 1);
+      this.charindex++;
 
-  // typeEffect(): void {
-  //   const currentWord = this.words[this.wordindex];
+      if (this.charindex > currentWord.length) {
+        this.isDeleting = true;
 
-  //   if (this.isDeleting) {
-  //     this.displaytext = currentWord.substring(0, this.charindex - 1)
-  //     this.charindex--;
+        setTimeout(() => this.typeEffect(), 1500);
 
-  //     if (this.charindex < 0) {
-  //       this.isDeleting = false;
-  //       this.wordindex = (this.wordindex + 1) % this.words.length;
-  //       this.charindex = 0;
-  //     }
-  //   }
-
-  //   else {
-  //     this.displaytext = currentWord.substring(0, this.charindex + 1);
-  //     this.charindex++;
-
-  //     if (this.charindex > currentWord.length) {
-  //       this.isDeleting = true;
-
-  //       setTimeout(() => this.typeEffect(), 1500);
-
-  //       return;
-  //     }
-  //   }
-  //   setTimeout(() => {
-  //     this.typeEffect();
-  //   }, this.isDeleting ? 60 : 100);
-  // }
+        return;
+      }
+    }
+    setTimeout(() => {
+      this.typeEffect();
+    }, this.isDeleting ? 60 : 100);
+  }
 }
