ce913a6e5a1f8e8493bf0fbc0552d2dd30e5c50f
Hitesh Vekariya <hiteshvekariya012@gmail.com>
2026-06-10 10:42:31 +0530

    update

--- src/app/My_Component/home/home.component.ts
+++ src/app/My_Component/home/home.component.ts
@@ -10,6 +10,40 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 })
 export class HomeComponent {
 
-  
+ words : String[] = [
+  "Frontend Developer",
+  "Web Desinger"
+ ]
+
+ displaytext ="";
+ wordindex = 0;
+ charindex =0;
+ isDeleting = false;
+
+ typeEffect() {
+  const currentWord = this.words[this.wordindex];
+
+  if(!this.isDeleting){
+    this.displaytext = currentWord.substring(0 , this.charindex + 1)
+    this.charindex++;
+
+    if(this.charindex === currentWord.length){
+      this.isDeleting = false;
+      setTimeout(()=>{this.typeEffect(),1500});
+      return;
+    }
+  }
+
+  else{
+    this.displaytext = currentWord.substring(0 , this.charindex - 1)
+    this.charindex--;
+
+    if(this.charindex === 0){
+      this.isDeleting = false;
+      this.wordindex = (this.wordindex + 1) % this.words.length;
+    }
+  }
+  setTimeout(() => this.typeEffect() , this.isDeleting ? 60 : 100);
+ } 
 
 };
