48a65117b9b38e107ae619afbed22add05784375
Hitesh Vekariya <hiteshvekariya012@gmail.com>
2026-06-10 13:18:09 +0530

    Update

--- src/app/My_Component/home/home.component.ts
+++ src/app/My_Component/home/home.component.ts
@@ -20,37 +20,61 @@ export class HomeComponent implements OnInit {
   isDeleting = false;
 
   ngOnInit(): void {
-    this.typeEffect();
-  }
+  this.startTyping();
+}
 
-  typeEffect(): void {
+startTyping() {
+  setInterval(() => {
     const currentWord = this.words[this.wordindex];
 
-    if (this.isDeleting) {
-      this.displaytext = currentWord.substring(0, this.charindex - 1)
-      this.charindex--;
-
+    if (!this.isDeleting) {
+      this.displaytext = currentWord.substring(0, this.charindex++);
+      if (this.charindex > currentWord.length) {
+        this.isDeleting = true;
+      }
+    } else {
+      this.displaytext = currentWord.substring(0, this.charindex--);
       if (this.charindex < 0) {
         this.isDeleting = false;
         this.wordindex = (this.wordindex + 1) % this.words.length;
         this.charindex = 0;
       }
     }
+  }, 100);
+}
 
-    else {
-      this.displaytext = currentWord.substring(0, this.charindex + 1);
-      this.charindex++;
+  // ngOnInit(): void {
+  //   this.typeEffect();
+  // }
 
-      if (this.charindex > currentWord.length) {
-        this.isDeleting = true;
+  // typeEffect(): void {
+  //   const currentWord = this.words[this.wordindex];
 
-        setTimeout(() => this.typeEffect(), 1500);
+  //   if (this.isDeleting) {
+  //     this.displaytext = currentWord.substring(0, this.charindex - 1)
+  //     this.charindex--;
 
-        return;
-      }
-    }
-    setTimeout(() => {
-      this.typeEffect();
-    }, this.isDeleting ? 60 : 100);
-  }
+  //     if (this.charindex < 0) {
+  //       this.isDeleting = false;
+  //       this.wordindex = (this.wordindex + 1) % this.words.length;
+  //       this.charindex = 0;
+  //     }
+  //   }
+
+  //   else {
+  //     this.displaytext = currentWord.substring(0, this.charindex + 1);
+  //     this.charindex++;
+
+  //     if (this.charindex > currentWord.length) {
+  //       this.isDeleting = true;
+
+  //       setTimeout(() => this.typeEffect(), 1500);
+
+  //       return;
+  //     }
+  //   }
+  //   setTimeout(() => {
+  //     this.typeEffect();
+  //   }, this.isDeleting ? 60 : 100);
+  // }
 }
