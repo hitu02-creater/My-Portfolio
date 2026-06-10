a3a7b3a6ce095c8e6808a245ffe4e5e24a55d503
Hitesh Vekariya <hiteshvekariya012@gmail.com>
2026-06-10 11:26:23 +0530

    changes

--- src/app/My_Component/home/home.component.ts
+++ src/app/My_Component/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component , OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,44 +9,49 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent implements OnInit {
 
- words : String[] = [
-  "Frontend Developer",
-  "Web Desinger"
- ]
+  words: String[] = [
+    "Frontend Developer",
+    "Web Desinger"
+  ]
 
- displaytext ="";
- wordindex = 0;
- charindex =0;
- isDeleting = false;
+  displaytext = "";
+  wordindex = 0;
+  charindex = 0;
+  isDeleting = false;
 
- ngOnInit(): void {
+  ngOnInit(): void {
     this.typeEffect();
   }
 
- typeEffect() {
-  const currentWord = this.words[this.wordindex];
+  typeEffect(): void {
+    const currentWord = this.words[this.wordindex];
 
-  if(!this.isDeleting){
-    this.displaytext = currentWord.substring(0 , this.charindex + 1)
-    this.charindex++;
+    if (this.isDeleting) {
+      this.displaytext = currentWord.substring(0, this.charindex - 1)
+      this.charindex--;
 
-    if(this.charindex === currentWord.length){
-      this.isDeleting = true;
-      setTimeout(()=>this.typeEffect(),1500);
-      return;
+      if (this.charindex === 0) {
+        this.isDeleting = false;
+        this.wordindex = (this.wordindex + 1) % this.words.length;
+      }
     }
-  }
 
-  else{
-    this.displaytext = currentWord.substring(0 , this.charindex - 1)
-    this.charindex--;
+    else {
+      this.displaytext = currentWord.substring(0, this.charindex + 1);
+      this.charindex++;
 
-    if(this.charindex === 0){
-      this.isDeleting = false;
-      this.wordindex = (this.wordindex + 1) % this.words.length;
+      if (this.charindex === currentWord.length) {
+        this.isDeleting = true;
+
+        setTimeout(() => {
+          this.typeEffect();
+        }, 1500);
+
+        return;
+      }
     }
+    setTimeout(() => {
+      this.typeEffect();
+    }, this.isDeleting ? 60 : 100);
   }
-  setTimeout(() => this.typeEffect() , this.isDeleting ? 60 : 100);
- } 
-
-};
+}
